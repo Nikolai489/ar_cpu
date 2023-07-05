1e1cb20fa45cc8d2ccc10e26b1b0e3d7d3e48fbd
Nikolai489 <mohitvaidya48@gmail.com>
2023-07-05 12:55:51 -0700

    Write a testbench for branch predictor

---
+++ tests/unit_test/dr32e_branch_predict/components/instr_defs.h
@@ -0,0 +1,9 @@
+#define JAL 0xFFFFFF6F
+#define BEQ 0xFFFF8FE3
+#define BNE 0xFFFF9FE3
+#define BLT 0xFFFFCFE3
+#define BGE 0xFFFFDFE3
+#define BLTU 0xFFFFEFE3
+#define BGEU 0xFFFFFFE3
+
+#define BRANCH 0x63
